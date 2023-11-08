0c5d80f59c095a3ac4d685ad630d2730d4091489
tysunday <120739860+tysunday@users.noreply.github.com>
2023-11-08 09:32:48 +0500

    Add files via upload

--- hw_07.11.2023_Builder_Sailor_Pilot.cs
+++ hw_07.11.2023_Builder_Sailor_Pilot.cs
@@ -19,7 +19,7 @@ namespace hw_07._11._2023_Builder_Sailor_Pilot
 
         public Human() { }
         public Human(string firstName, string lastName)
-        { 
+        {
             this.firstName = firstName;
             this.lastName = lastName;
         }
@@ -33,19 +33,21 @@ namespace hw_07._11._2023_Builder_Sailor_Pilot
                 $"First name: {firstName}\n" +
                 $"Date of birth: {birthDate.ToShortDateString()}\n";
         }
+        public abstract void Introduce();
     }
 
     public abstract class Employee : Human
     {
         decimal salary;
+
         public Employee(string firstName, string lastName) : base(firstName, lastName) { }
         public Employee(string firstName, string lastName, decimal salary) : base(firstName, lastName)
         {
             this.salary = salary;
         }
-        public Employee(string firstName, string lastName, DateTime date, decimal salary) :
-            base(firstName, lastName, date)
-        { 
+        public Employee(string firstName, string lastName, DateTime date, decimal salary) 
+            : base(firstName, lastName, date)
+        {
             this.salary = salary;
         }
         public override string ToString()
@@ -53,11 +55,82 @@ namespace hw_07._11._2023_Builder_Sailor_Pilot
             return base.ToString() + $"Salary: {salary} ruble\n";
         }
     }
+
+    class Builder : Employee
+    {
+        int numberOfHousesBuilt;
+        public Builder(string firstName, string lastName, DateTime date, decimal salary, int numberOfHousesBuilt)
+            : base(firstName, lastName, date, salary)
+        {
+            this.numberOfHousesBuilt = numberOfHousesBuilt;
+        }
+
+        public override void Introduce()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"I built many buildings, there are {numberOfHousesBuilt} in total.\n";
+        }
+    }
+
+    class Sailor : Employee
+    {
+        int numberOfMilesSailedBySailor;
+
+        public Sailor(string firstName, string lastName, DateTime date, decimal salary, int numberOfMilesSailedBySailor) 
+            : base(firstName, lastName, date, salary)
+        {
+            this.numberOfMilesSailedBySailor = numberOfMilesSailedBySailor;
+        }
+
+        public override void Introduce()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Sailor. I have sailed many miles, about {numberOfMilesSailedBySailor}.\n";
+        }
+    }
+
+    class Pilot : Employee
+    {
+        int targetsHit;
+        public Pilot(string firstName, string lastName, DateTime date, decimal salary, int targetsHit)
+           : base(firstName, lastName, date, salary)
+        {
+            this.targetsHit = targetsHit;
+        }
+
+        public override void Introduce()
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"Pilot. Destroyed {targetsHit} targets.\n";
+        }
+    }
+
     internal class Program
     {
         static void Main()
         {
+            Employee[] employees =
+            {
+                new Builder("Oleg", "Olegov", new DateTime(1220, 05, 29), 5, 3),
+                new Sailor("Morykov", "Moryak", new DateTime(0005, 09, 09), 0.4m, 38348),
+                new Pilot("Vladislav", "Vladislavov", new DateTime(2001, 10, 18), 0.1m, 7)
+            };
 
+            foreach(Employee emp in employees)
+            {
+                Console.WriteLine(emp);
+            }
         }
     }
 }
