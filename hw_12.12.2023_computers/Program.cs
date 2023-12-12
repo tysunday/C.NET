bf6ae0c947309bfb24ed704127df09ea9fd7fb84
tysunday <120739860+tysunday@users.noreply.github.com>
2023-12-12 16:59:49 +0500

    Add files via upload

---
+++ hw_12.12.2023_computers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComputersDll;
+
+namespace hw_12._12._2023_computers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
